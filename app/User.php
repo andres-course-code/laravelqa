6dac761bed6c9a15f9c75cf0796849c3dcdd0633
Samuel <aandres,7xlol@gmail.com>
2020-06-09 19:56:47 -0500

    se instala debugbar y se hace eager load

--- app/User.php
+++ app/User.php
@@ -31,4 +31,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+
+    public function getUrlAttribute()
+    {
+        //return route('question.show',$this->id);
+        return '#';
+    }
+
 }
